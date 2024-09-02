2751f51d90f251f1872fbba9d1c48d20bf72c766
zhangxiang.chen-EFWOM <1026161550@qq.com>
2024-09-02 11:11:39 +0800

    Revert "fix bug: no available backend found. ERR: [webgpu] [object ErrorEvent]不再使用PWA"
    This reverts commit 6534b24533da08ee28fafb54accd7e63819099fc.

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
@@ -9,5 +10,33 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'RMBG-webui',
+      short_name: 'RMBG-webui',
+      description: '基于RMBG-1.4模型的PWA应用',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
 })
